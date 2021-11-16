ea0d566867699cece071a5a25926130ee08a4ff7
VSGic <vsgrebnev@yandex.ru>
2021-11-16 22:39:05 +0300

    Added netmask as argument

--- netmaska.py
+++ netmaska.py
@@ -1,6 +1,11 @@
-
 from bin import netmask_analyzer_engine
+import sys
 
-input_netmask = input('Input netmask for analysis:')
+if len(sys.argv) > 1 and len(sys.argv) < 3: 
+	input_netmask = sys.argv[1]
+else: 
+	input_netmask = input('Input netmask for analysis:')
+
+
 netmask_analyzer_engine.input_process(input_netmask)
 
